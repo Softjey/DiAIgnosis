f49413240ea9138e1c6eb95060fd3591da79a03f
Softjey <misivsvatoslav@gmail.com>
2024-04-17 23:00:28 +0200

    add base template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@nextui-org/button'
+
+function App() {
+
+  return (
+    <>
+      <Button variant='flat' size='lg' color='warning'>Start Hackathon</Button>
+    </>
+  )
+}
+
+export default App
