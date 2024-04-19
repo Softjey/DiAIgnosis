e62081b88c122d134d141838e51c18bb61fd3860
Softjey <misivsvatoslav@gmail.com>
2024-04-19 13:26:40 +0200

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { NextUIProvider } from '@nextui-org/react';
-import App from "./App.tsx";
+import { NextUIProvider } from "@nextui-org/react";
+import {
+  RouterProvider,
+  createBrowserRouter as createRouter,
+} from "react-router-dom";
 import "./index.css";
+import App from "./App";
+
+const router = createRouter([
+  {
+    path: "",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </React.StrictMode>
 );
