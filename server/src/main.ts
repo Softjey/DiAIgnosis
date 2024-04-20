5505ea910734a0492e340c076168c453b1ef64ca
Softjey <misivsvatoslav@gmail.com>
2024-04-20 11:20:43 +0200

    try to fix cors 3

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://softjey.github.io', 'https://junk-debug.github.io'],
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    preflightContinue: true,
   });
 
   app.useGlobalPipes(
