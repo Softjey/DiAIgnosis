4677b1d53a60985b79146cb27902dcc39f3213d9
Softjey <misivsvatoslav@gmail.com>
2024-04-20 06:37:42 +0200

    add results logic, fixes

--- server/src/consultation/consultation.service.ts
+++ server/src/consultation/consultation.service.ts
@@ -9,17 +9,44 @@ import { Consultation } from './interfaces/consultation.interface';
 import { generateExtraQuestionsPrompt } from './prompts/generate-extra-questions';
 import { Question } from './interfaces/question.interface';
 import { ChatCompletionCreateParamsBase } from 'openai/resources/chat/completions';
+import { getResultsPrompt } from './prompts/get-results';
+import { diagnosisDoctorsPrompt } from './prompts/diagnosis-doctors';
 
 @Injectable()
 export class ConsultationService {
   private gptModel: ChatCompletionCreateParamsBase['model'] = 'gpt-3.5-turbo-0125';
   private startPrompt = this.openAiService.systemPrompt(generateStartQuestionsPrompt);
   private continuePrompt = this.openAiService.systemPrompt(generateExtraQuestionsPrompt);
+  private resultsPrompt = this.openAiService.systemPrompt(getResultsPrompt);
+  private diagnosisDoctorsPrompt = this.openAiService.systemPrompt(diagnosisDoctorsPrompt);
 
   constructor(private readonly openAiService: OpenAiService) {}
 
+  private async getJSONResponse(messages: ChatCompletionCreateParamsBase['messages']) {
+    const response = await this.openAiService.chatGptRequest({
+      messages,
+      model: this.gptModel,
+      response_format: { type: 'json_object' },
+    });
+
+    return JSON.parse(response);
+  }
+
   private prepareForClient(questions: Question[]) {
     return questions.map(({ id, text }) => ({ id, text }));
+  }
+
+  private getMessagesForGpt(questions: Consultation['questions']) {
+    const questionsValues = Array.from(questions.values());
+
+    return questionsValues
+      .map(({ text, answer }) => {
+        const role = 'user' as const;
+        const content = `Question: ${text} Answer: ${answer}`;
+
+        return [{ role, content }];
+      })
+      .flat();
   }
 
   private mapToQuestion(questions: string[]): Question[] {
@@ -30,14 +57,38 @@ export class ConsultationService {
     }));
   }
 
-  private async generateQuestions(patientComplaint: string) {
-    const response = await this.openAiService.chatGptRequest({
-      messages: [...this.startPrompt, { role: 'user', content: patientComplaint }],
-      model: this.gptModel,
-      response_format: { type: 'json_object' },
-    });
+  private async generateResults(consultation: Consultation) {
+    const messages = this.getMessagesForGpt(consultation.questions);
 
-    const { questions, error } = JSON.parse(response);
+    const { results } = await this.getJSONResponse([
+      ...this.startPrompt,
+      ...messages,
+      ...this.resultsPrompt,
+    ]);
+
+    return results as Record<string, string>;
+  }
+
+  private async generateDiagnosisAndDoctors(consultation: Consultation) {
+    const messages = this.getMessagesForGpt(consultation.questions);
+
+    const { diagnosis, doctors } = await this.getJSONResponse([
+      ...this.startPrompt,
+      ...messages,
+      ...this.diagnosisDoctorsPrompt,
+    ]);
+
+    return {
+      diagnosis: diagnosis as string[],
+      doctors: doctors as string[],
+    };
+  }
+
+  private async generateQuestions(patientComplaint: string) {
+    const { questions, error } = await this.getJSONResponse([
+      ...this.startPrompt,
+      { role: 'user', content: patientComplaint },
+    ]);
 
     if (error) {
       throw new BadRequestException(
@@ -45,29 +96,16 @@ export class ConsultationService {
       );
     }
 
-    console.log('questions', questions);
-
     return questions as string[];
   }
 
   private async generateExtraQuestions(consultation: Consultation) {
-    const questionsValues = Array.from(consultation.questions.values());
-    const messages = questionsValues
-      .map(({ text, answer }) => [
-        {
-          role: 'user' as const,
-          content: `Question: ${text} Answer: ${answer}`,
-        },
-      ])
-      .flat();
-
-    const response = await this.openAiService.chatGptRequest({
-      messages: [...this.startPrompt, ...messages, ...this.continuePrompt],
-      model: this.gptModel,
-      response_format: { type: 'json_object' },
-    });
-
-    const { questions, end } = JSON.parse(response);
+    const messages = this.getMessagesForGpt(consultation.questions);
+    const { questions, end } = await this.getJSONResponse([
+      ...this.startPrompt,
+      ...messages,
+      ...this.continuePrompt,
+    ]);
 
     if (end) {
       return {
@@ -91,6 +129,8 @@ export class ConsultationService {
       questions: new Map(questions.map((question) => [question.id, question])),
     });
 
+    console.log('consultationId', consultationId);
+
     return {
       consultationId,
       questions: this.prepareForClient(questions),
@@ -113,12 +153,21 @@ export class ConsultationService {
     }
 
     answers.forEach(({ questionId, answer }) => {
-      if (!consultation.questions.has(questionId)) {
-        throw new NotFoundException(`Question with id ${questionId} not found`);
+      const question = consultation.questions.get(questionId);
+
+      if (!question || question.answer) {
+        throw new NotFoundException(`Question with id ${questionId} not found or already answered`);
       }
 
       consultation.questions.get(questionId).answer = answer;
     });
+
+    if (consultation.questions.size > 4) {
+      return {
+        status: 'ended',
+        questions: null,
+      };
+    }
 
     const { questions: textQuestions, status } = await this.generateExtraQuestions(consultation);
 
@@ -131,9 +180,32 @@ export class ConsultationService {
 
     const questions = this.mapToQuestion(textQuestions);
 
+    questions.forEach((question) => {
+      consultation.questions.set(question.id, question);
+    });
+
     return {
       status,
       questions: this.prepareForClient(questions),
+    };
+  }
+
+  async getResults(consultationId: string) {
+    const consultation = consultationStorage.get(consultationId);
+
+    if (!consultation) {
+      throw new NotFoundException('Consultation not found');
+    }
+
+    const [results, { diagnosis, doctors }] = await Promise.all([
+      this.generateResults(consultation),
+      this.generateDiagnosisAndDoctors(consultation),
+    ]);
+
+    return {
+      results,
+      diagnosis,
+      doctors,
     };
   }
 }
