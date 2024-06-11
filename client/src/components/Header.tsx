62de11f9613898ca04ffd3d8404fbefb0463d13c
Softjey <misivsvatoslav@gmail.com>
2024-06-11 14:48:41 +0200

    fix header styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import logoUrl from "/logo.svg";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex w-full justify-start items-center gap-3 absolute top-0 left-0 px-10 py-5 header">
+    <header className="flex w-full justify-start items-center gap-3 absolute top-0 left-0 px-10 py-5 select-none">
       <img src={logoUrl} alt="" />
 
       <h3 className="text-black text-3xl">DiAIgnosis</h3>
